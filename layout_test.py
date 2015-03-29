af3da091742d72363d479ef344bb9afd92591566
Chris Purcell <cpurcell@palantir.com>
2015-03-29 14:07:47 +0100

    Clean up layout_test.py

--- layout_test.py
+++ layout_test.py
@@ -49,156 +49,79 @@ def test_row_max_min_cols():
   assert Row(2, through={1,5})._max == 2
   assert Row(2, through={1,5})._cols == 6
 
-class TestBranchMerge(object):
+def test_layout_multi_branch_merge():
+  gh_pages = Node("gh-pages")
+  master = Node("master")
+  footnotes = Node("footnotes", master, Node("origin/footnotes"))
+  issue4 = Node("issue4.idiomatic.java7", master)
+  test_logs = Node("cleaner.test.logs", master)
+  issue29 = Node("issue29-wildcards-in-optional-types", master)
+  cleanup = Node("cleanup", master)
+  issue2 = Node("issue2.nulls", master)
+  issue27 = Node("issue27.dogfood", cleanup, issue2)
+  develop = Node("develop", issue4, test_logs, issue29, issue27)
+  refactor = Node("code.generator.refactor", develop)
+  issue24 = Node("issue24.nested.classes.last", refactor)
+  excerpt = Node("excerpt", issue24)
+  qualified_name = Node("qualified.name", excerpt)
+  no_warnings = Node("assert.no.warnings", qualified_name)
+  issue1 = Node("issue1.generics", no_warnings)
 
-  def setUp(self):
-    self.expected = dedent(u"""\
-                ┬  issue1.generics
-                ┼  assert.no.warnings
-                ┼  qualified.name
-                ┼  excerpt
-                ┼  issue24.nested.classes.last
-                ┼  code.generator.refactor
-          ┌─┬─┬▶┤  develop
-        ┌┄│┄│┄│▶┤  issue27.dogfood
-        ┼ │ │ │ │  issue2.nulls
-        ├┄│┄│┄│▶┘  cleanup
-        ├┄│┄│▶┘  issue29-wildcards-in-optional-types
-        ├┄│▶┘  cleaner.test.logs
-        ├▶┘  issue4.idiomatic.java7
-        ├▶╴  footnotes
-        ┴  master
-        ─  gh-pages
-    """)
+  branches = [ issue1, no_warnings, qualified_name, excerpt, issue24, refactor, develop,
+               issue27, issue2, cleanup, issue29, test_logs, issue4, footnotes, master,
+               gh_pages ]
+  assert layout(branches) == [
+      Row(at = 4, down = [4]),
+      Row(at = 4, up = [4], down = [4]),
+      Row(at = 4, up = [4], down = [4]),
+      Row(at = 4, up = [4], down = [4]),
+      Row(at = 4, up = [4], down = [4]),
+      Row(at = 4, up = [4], down = [4]),
+      Row(at = 4, up = [4], down = [1,2,3,4]),
+      Row(at = 4, up = [4], down = [0,4], through = [1,2,3]),
+      Row(at = 0, up = [0], down = [0], through = [1,2,3,4]),
+      Row(at = 4, up = [0,4], down = [0], through = [1,2,3]),
+      Row(at = 3, up = [0,3], down = [0], through = [1,2]),
+      Row(at = 2, up = [0,2], down = [0], through = [1]),
+      Row(at = 1, up = [0,1], down = [0]),
+      Row(at = 1, up = [0], down = [0]),
+      Row(at = 0, up = [0]),
+      Row(at = 0),
+  ]
 
-    gh_pages = Node("gh-pages")
-    master = Node("master")
-    footnotes = Node("footnotes", master, Node("origin/footnotes"))
-    issue4 = Node("issue4.idiomatic.java7", master)
-    test_logs = Node("cleaner.test.logs", master)
-    issue29 = Node("issue29-wildcards-in-optional-types", master)
-    cleanup = Node("cleanup", master)
-    issue2 = Node("issue2.nulls", master)
-    issue27 = Node("issue27.dogfood", cleanup, issue2)
-    develop = Node("develop", issue4, test_logs, issue29, issue27)
-    refactor = Node("code.generator.refactor", develop)
-    issue24 = Node("issue24.nested.classes.last", refactor)
-    excerpt = Node("excerpt", issue24)
-    qualified_name = Node("qualified.name", excerpt)
-    no_warnings = Node("assert.no.warnings", qualified_name)
-    issue1 = Node("issue1.generics", no_warnings)
+def test_layout_simple_merge_with_crossover():
+  develop = Node("develop")
+  autovalue = Node("feature/auto.value", develop)
+  deadlock = Node("feature/deadlock.transfercontroller", develop)
+  workshop = Node("workshop", autovalue, deadlock)
+  freebuilder = Node("feature/freebuilder", autovalue)
 
-    self.branches = [ issue1, no_warnings, qualified_name, excerpt, issue24, refactor, develop,
-                      issue27, issue2, cleanup, issue29, test_logs, issue4, footnotes, master,
-                      gh_pages ]
-    self.grid = [
-        Row(at = 4, down = [4]),
-        Row(at = 4, up = [4], down = [4]),
-        Row(at = 4, up = [4], down = [4]),
-        Row(at = 4, up = [4], down = [4]),
-        Row(at = 4, up = [4], down = [4]),
-        Row(at = 4, up = [4], down = [4]),
-        Row(at = 4, up = [4], down = [1,2,3,4]),
-        Row(at = 4, up = [4], down = [0,4], through = [1,2,3]),
-        Row(at = 0, up = [0], down = [0], through = [1,2,3,4]),
-        Row(at = 4, up = [0,4], down = [0], through = [1,2,3]),
-        Row(at = 3, up = [0,3], down = [0], through = [1,2]),
-        Row(at = 2, up = [0,2], down = [0], through = [1]),
-        Row(at = 1, up = [0,1], down = [0]),
-        Row(at = 1, up = [0], down = [0]),
-        Row(at = 0, up = [0]),
-        Row(at = 0),
-    ]
+  branches = [ freebuilder, workshop, deadlock, autovalue, develop ]
+  assert layout(branches) == [
+      Row(at = 1, down = [1]),
+      Row(at = 2, up = [1], down = [0,1]),
+      Row(at = 0, up = [0], down = [0], through = [1]),
+      Row(at = 1, up = [0,1], down = [0]),
+      Row(at = 0, up = [0]),
+  ]
 
-  def test_grid(self):
-    self.setUp()
-    grid = layout(self.branches)
-    assert grid == self.grid
+def test_layout_simple_merge_no_crossover():
+  develop = Node("develop")
+  autovalue = Node("feature/auto.value", develop)
+  deadlock = Node("feature/deadlock.transfercontroller", develop)
+  freebuilder = Node("feature/freebuilder", autovalue)
+  workshop = Node("workshop", autovalue, deadlock)
 
-  def test_output(self):
-    self.setUp()
-    file = StringIO()
-    layout_to(file, self.branches)
-    output = dedent(file.getvalue())
-    assert output == self.expected
+  branches = [ workshop, freebuilder, autovalue, deadlock, develop ]
+  assert layout(branches) == [
+      Row(at = 1, down = [0,1]),
+      Row(at = 2, up = [0], down = [0], through = [1]),
+      Row(at = 0, up = [0], down = [0], through = [1]),
+      Row(at = 1, up = [0,1], down = [0]),
+      Row(at = 0, up = [0]),
+  ]
 
-class TestSimpleMergeWithCrossover(object):
-
-  def setUp(self):
-    self.expected = dedent(u"""\
-          ┬  feature/freebuilder
-        ┌─┼▶╴  workshop
-        ┼ │  feature/deadlock.transfercontroller
-        ├▶┘  feature/auto.value
-        ┴  develop
-    """)
-
-    develop = Node("develop")
-    autovalue = Node("feature/auto.value", develop)
-    deadlock = Node("feature/deadlock.transfercontroller", develop)
-    workshop = Node("workshop", autovalue, deadlock)
-    freebuilder = Node("feature/freebuilder", autovalue)
-
-    self.branches = [ freebuilder, workshop, deadlock, autovalue, develop ]
-    self.grid = [
-        Row(at = 1, down = [1]),
-        Row(at = 2, up = [1], down = [0,1]),
-        Row(at = 0, up = [0], down = [0], through = [1]),
-        Row(at = 1, up = [0,1], down = [0]),
-        Row(at = 0, up = [0]),
-    ]
-
-  def test_grid(self):
-    self.setUp()
-    grid = layout(self.branches)
-    assert grid == self.grid
-
-  def test_output(self):
-    self.setUp()
-    file = StringIO()
-    layout_to(file, self.branches)
-    output = dedent(file.getvalue())
-    assert output == self.expected
-
-class TestSimpleMergeNoCrossover(object):
-
-  def setUp(self):
-    self.expected = dedent(u"""\
-        ┌▶┐  workshop
-        ├┄│▶╴  feature/freebuilder
-        ┼ │  feature/auto.value
-        ├▶┘  feature/deadlock.transfercontroller
-        ┴  develop
-    """)
-
-    develop = Node("develop")
-    autovalue = Node("feature/auto.value", develop)
-    deadlock = Node("feature/deadlock.transfercontroller", develop)
-    freebuilder = Node("feature/freebuilder", autovalue)
-    workshop = Node("workshop", autovalue, deadlock)
-    self.branches = [ workshop, freebuilder, autovalue, deadlock, develop ]
-
-    self.grid = [
-        Row(at = 1, down = [0,1]),
-        Row(at = 2, up = [0], down = [0], through = [1]),
-        Row(at = 0, up = [0], down = [0], through = [1]),
-        Row(at = 1, up = [0,1], down = [0]),
-        Row(at = 0, up = [0]),
-    ]
-
-  def test_grid(self):
-    self.setUp()
-    grid = layout(self.branches)
-    assert grid == self.grid
-
-  def test_output(self):
-    self.setUp()
-    file = StringIO()
-    layout_to(file, self.branches)
-    output = dedent(file.getvalue())
-    assert output == self.expected
-
-def test_branch_merge_to_head():
+def test_row_unicode_branch_merge_to_head():
   grid = [
       Row(at = 0, down = [0]),
       Row(at = 0, up = [0], down = [0]),
@@ -237,7 +160,7 @@ def test_branch_merge_to_head():
       ─
   """)
 
-def test_remerge_to_head():
+def test_row_unicode_remerge_to_head():
   grid = [
       Row(at = 1, down = [1]),
       Row(at = 0, up = [1], down = [0,1]),
@@ -254,7 +177,7 @@ def test_remerge_to_head():
       ┴
   """)
 
-def test_simple_merge_to_head_with_crossunder():
+def test_row_unicode_simple_merge_to_head_with_crossunder():
   grid = [
       Row(at = 0, down = [0,1]),
       Row(at = 2, up = [0], down = [0], through=[1]),
